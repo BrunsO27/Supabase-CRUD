90583dcd1f4f264ea58d7d035d18ad0a9165fe9e
Bruno Ortiz <bruno.ortiz_a10@ciencias.unam.mx>
2026-01-04 20:53:01 -0600

    :recycle: Adaptacion de la pagina principal

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,24 @@
 <template>
-    <h1> Pagina home </h1>
+    <div class="min-h-screen flex flex-col bg-gray-50">
+        <Header @nueva-tarea="handleNuevaTarea" />
+
+        <main class="container mx-auto py-8 px-4 max-w-6xl">
+            <TaskTable ref="taskTableRef" />
+        </main>
+    </div>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+import Header from '@/components/Header.vue';
+import TaskTable from '@/components/TaskTable.vue';
+
+const taskTableRef = ref(null);
+
+const handleNuevaTarea = () => {
+    console.log('Recargando tabla desde HomeView...');
+    if (taskTableRef.value) {
+        taskTableRef.value.cargarTareas();
+    }
+};
+</script>
